f14ee9bf1ffdbceb099a48715103eb955b92835c
20CThomas <cthomcoding@gmail.com>
2025-03-28 19:29:56 -0700

    fix: remove defaultTab prop from OverviewTabs usage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <div className="flex-1 flex flex-col">
         <Header />
         <main className="flex-1 overflow-y-auto p-4">
-          <OverviewTabs defaultTab="overview" />
+          <OverviewTabs />
         </main>
         <Footer className="mt-auto" />
       </div>
